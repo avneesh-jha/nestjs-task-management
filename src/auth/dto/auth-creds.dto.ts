919ba9ee3aa864703e0473472bd630446829c909
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-05-06 23:59:14 +0530

    password validation and user name length

--- src/auth/dto/auth-creds.dto.ts
+++ src/auth/dto/auth-creds.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+  @MinLength(8)
+  @MaxLength(16)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak!',
+  })
   password: string;
 }
