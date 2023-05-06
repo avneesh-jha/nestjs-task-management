066f883580eb35ad8d071368bedf374213fb82de
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-05-07 01:24:41 +0530

    error handling for making sure unique  username is used

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,25 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Not, Repository } from 'typeorm';
 import { AuthCredsDto } from './dto/auth-creds.dto';
 import { User } from './user.entitiy';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredsDto: AuthCredsDto): Promise<void> {
     const { username, password } = authCredsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (e) {
+      console.log(e.code);
+      if (e.code == 23505) {
+        throw new ConflictException('User name is duplicate');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
