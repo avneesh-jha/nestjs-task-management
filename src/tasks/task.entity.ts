49bdb1d7712d584b8b8ade1181638642f601e1bb
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-05-12 00:16:04 +0530

    apllied interceptor to avoid sensitive data from resposne

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { statusType } from './task.model';
 import { User } from 'src/auth/user.entitiy';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task {
@@ -17,5 +18,6 @@ export class Task {
   status: statusType;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
